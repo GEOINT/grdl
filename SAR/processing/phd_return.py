6b0a3d2a47f665c6a0174bf2a8714d2c770a83d9
BD: BEEF DADDY <duane.d.smalley@gmail.com>
2024-10-02 10:43:29 -0400

    updated

--- SAR/processing/phd_return.py
+++ SAR/processing/phd_return.py
@@ -5,11 +5,20 @@ Created on Sat Aug  3 12:16:54 2024
 @author: User
 """
 import numpy as np
-class phd_return():
+class PHD_Return():
     '''
     This class is meant to parse the meta data of SICD and return the valid regions
     of the data. You would want to use this for the purpose of making the subapertures
     of the psuedo phase history data from a SICD
+    dependency:
+        SARPy
+        Numpy
+    
+    usage:
+        
+        from sarpy.io.complex.sicd import SICDReader
+        
+    
     '''
     def __init__(self, meta):
         self.meta = meta
@@ -24,7 +33,7 @@ class phd_return():
         self.az_os_pixel_frac = 1 - 1/az_oversample
         a = 1
         
-    def get_phd_pixels(self, chip_shape):
+    def get_phd_data_pixel_range(self, chip_shape):
         '''
 
         Parameters
@@ -42,7 +51,8 @@ class phd_return():
         cstart : int
             the column start position of where the data begins. This is the span into
             the phase history data of where the zero padded region is.
-
+        its assumed that the data start pixel is then can be cast to the end pixel with
+        the length of the row - rstart.
         '''
         nrows = chip_shape[0]
         ncols = chip_shape[1]
@@ -50,7 +60,19 @@ class phd_return():
         rstart = np.round( nrows*self.rg_os_pixel_frac/2 ).astype(int)
         cstart = np.round( ncols*self.az_os_pixel_frac/2 ).astype(int)
         
-        return rstart, cstart
+        rend = nrows - rstart
+        cend = ncols - cstart
+        
+        return [rstart, rend, cstart, cend]
+    
+    def get_phd_data( self, chip_data ):
+        from scipy import fft
+        
+        phd = np.fft.fftshift( fft.fft2( chip_data ) )
+        
+        return phd
+        
+    
         
         
         
